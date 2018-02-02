e69ecf32bbfa6351dd181035df7e27a7288802ea
albert0815 <dpapenberg@gmail.com>
2018-02-02 17:16:25 +0100

    include version

--- feinstaubr-web/src/main/java/de/feinstaubr/server/view/IndexViewController.java
+++ feinstaubr-web/src/main/java/de/feinstaubr/server/view/IndexViewController.java
@@ -1,5 +1,9 @@
 package de.feinstaubr.server.view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -14,13 +18,25 @@ public class IndexViewController {
 	@Inject
 	private Sensor sensor;
 	private SensorMeasurement currentSensorData;
+	private String version;
 	
 	@PostConstruct
 	public void init() {
 		currentSensorData = sensor.getCurrentSensorData("7620363");
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(IndexViewController.class.getResourceAsStream("/version")));
+			version = reader.readLine();
+		} catch (IOException e) {
+			version = "unknown";
+		}
+
 	}
 	
 	public SensorMeasurement getCurrentSensorData() {
 		return currentSensorData;
+	}
+	
+	public String getVersion() {
+		return version;
 	}
 }
