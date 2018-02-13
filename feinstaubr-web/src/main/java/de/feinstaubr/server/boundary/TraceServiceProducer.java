72ae2889dbcfbb1a04cf56a5d136a4ec43f7f82c
albert0815 <dpapenberg@gmail.com>
2018-02-13 15:17:22 +0100

    init tracer

--- feinstaubr-web/src/main/java/de/feinstaubr/server/boundary/TraceServiceProducer.java
+++ feinstaubr-web/src/main/java/de/feinstaubr/server/boundary/TraceServiceProducer.java
@@ -1,10 +1,16 @@
 package de.feinstaubr.server.boundary;
 
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+
+import com.google.cloud.trace.Trace;
+import com.google.cloud.trace.service.TraceGrpcApiService;
+import com.google.cloud.trace.service.TraceService;
 
 @Startup
 @Singleton
@@ -15,12 +21,12 @@ public class TraceServiceProducer {
 	public void setupTracerService() {
 		LOGGER.info("test of logging");
 		
-//	    try {			
-//	    	TraceService traceService = TraceGrpcApiService.builder()
-//		        .setProjectId("feinstaubr").build();
-//	    	Trace.init(traceService);
-//		} catch (IOException e) {
-//			LOGGER.log(Level.SEVERE, "unable to initialize trace service", e);
-//		}
+	    try {			
+	    	TraceService traceService = TraceGrpcApiService.builder()
+		        .setProjectId("feinstaubr").build();
+	    	Trace.init(traceService);
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "unable to initialize trace service", e);
+		}
 	}
 }
